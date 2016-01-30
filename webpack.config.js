432cef7b219160bc0f9f86a63ed900d5a187ef70
Slavomir Vojacek <slavomirvojacek@gmail.com>
2016-01-30 15:11:23 +0000

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path'),
+    webpack = require('webpack'),
+    WebpackNotifierPlugin = require('webpack-notifier');
+
+module.exports = {
+    entry: './src/main.tsx',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/assets/',
+        filename: 'app.js'
+    },
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        loaders: [
+            {test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules|lib/},
+            {test: /\.sass$/, loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'},
+            {test: /\.(png|gif|jpg|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000'},
+            {test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'}
+        ]
+    },
+    sassLoader: {
+        indentedSyntax: true
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new WebpackNotifierPlugin()
+    ]
+};
